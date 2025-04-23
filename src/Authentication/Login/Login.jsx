2fbe9076864f258bad2f2e1bb04e80bb4bb8b38e
Revanth0854 <revnth0854@gmail.com>
2025-04-23 20:38:52 +0530

    updated

--- src/Authentication/Login/Login.jsx
+++ src/Authentication/Login/Login.jsx
@@ -1,7 +1,8 @@
 
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import { fetchData } from './Redux/Slices/registerSlice';
 import { setLogin } from '../../Redux/Slices/registerSlice'
 import { ToastContainer, toast } from 'react-toastify'
 export const User = createContext()
@@ -32,36 +33,53 @@ const Login = () => {
     return true
   }
 
-  const handleLogin = (e) => {
+  const handleLogin =async (e) => {
     e.preventDefault()
 
-    if (!validateLogin) {
+    if (!validateLogin()) {
       return;
     }
-
-    const find = users.find(item => item.password === log.password && item.email === log.email)
-
-    const findAdmin = admin.find(item => item.email === log.email && item.password === log.password)
-
-    if (find) {
-      toast.success("Logged in Successfully")
-      dispatch(setLogin(find))
+    try{
+    const res = await axios.get(`https://redux-server-w0s1.onrender.com/users?email=${log.email}&password=${log.password}`);
+    const user = res.data[0];
+    if (user) {
+      toast.success("Logged in Successfully");
+      dispatch(setLogin(user));
       setTimeout(() => {
-        navigate('/home')
-      }, 1000)
+        navigate('/home');
+      }, 1000);
+    } else {
+      toast.error("Invalid Details");
     }
-    else if (findAdmin) {
-      toast.success("Welcome Admin")
-      setTimeout(() => {
-        navigate('/admin')
-      }, 1000)
-    }
-    else {
-      toast.error("Invalid Details")
-      alert("Email: revanth@gmail.com  Password: 123456")
-    }
+  } catch (err) {
+    console.error(err);
+    toast.error("Login failed. Please try again.");
   }
+    // const find = users.find(item => item.password === log.password && item.email === log.email)
 
+    // const findAdmin = admin.find(item => item.email === log.email && item.password === log.password)
+
+    // if (find) {
+    //   toast.success("Logged in Successfully")
+    //   dispatch(setLogin(find))
+    //   setTimeout(() => {
+    //     navigate('/home')
+    //   }, 1000)
+    // }
+    // else if (findAdmin) {
+    //   toast.success("Welcome Admin")
+    //   setTimeout(() => {
+    //     navigate('/admin')
+    //   }, 1000)
+    // }
+    // else {
+    //   toast.error("Invalid Details")
+    //   alert("Email: revanth@gmail.com  Password: 123456")
+    // }
+  }
+useEffect(()=>{
+  dispatch(fetchData())
+},[])
 
   return (
     <>
--- src/Redux/Slices/registerSlice.jsx
+++ src/Redux/Slices/registerSlice.jsx
@@ -1,33 +1,66 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
 // import data from "../../../../reduxServer/data.json"
 import axios from 'axios'
 const getData = fetch("https://redux-server-w0s1.onrender.com").then(res => res.json()).then(data1 => console.log(data1)).catch(err => console.log(err))
 
 
 const initialState = {
-    data: getData,
+    data: {
+        users:[],
+        admin:[]
+    },
     theme: false,
-    login: null
+    login: null,
+    loading:false,
+    error:null
 }
-
-const slice = createSlice({
+export const fetchData = createAsyncThunk(
+    'register/fetchData',
+    async () => {
+      const response = await axios.get('https://redux-server-w0s1.onrender.com');
+      return response.data;
+    }
+  );
+  
+  const slice = createSlice({
     name: "register",
     initialState,
     reducers: {
-        registerData: (state, action) => {
-            state.data.users = [...state.data.users, action.payload]
-        },
-        dark: (state) => {
-            state.theme = !state.theme
-        },
-        setLogin: (state, action) => {
-            state.login = action.payload
-        },
-        logout: (state) => {
-            state.login = null
-        }
+      registerData: (state, action) => {
+        state.data.users = [...state.data.users, action.payload];
+      },
+      dark: (state) => {
+        state.theme = !state.theme;
+      },
+      setLogin: (state, action) => {
+        state.login = action.payload;
+        localStorage.setItem("login", JSON.stringify(action.payload));
+      },
+      logout: (state) => {
+        state.login = null;
+        localStorage.removeItem("login");
+      }
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchData.pending, (state) => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(fetchData.fulfilled, (state, action) => {
+          state.loading = false;
+          state.data = action.payload;
+        })
+        .addCase(fetchData.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.error.message;
+        });
     }
-})
+  });
+  
+
+
 
 
 
